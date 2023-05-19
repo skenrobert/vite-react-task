638b3efc39c140761fbb29a21d7d81bf2cec1f65
skenrobert <skenrobert@gmail.com>
2023-05-19 18:19:55 -0500

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { AppContextProvider, AppContex } from './contex/AppContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
   </React.StrictMode>,
 )
