1cc7049340a74bb301bbd65ef33d4b3d03e3678d
skenrobert <skenrobert@gmail.com>
2023-05-19 17:50:36 -0500

    change for developer

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,35 @@
-import { useEffect, useState } from "react";
-import TaskForm from "./Components/TaskForm";
-import TaskList from "./Components/TaskList";
-import { tasks } from "./data/tasks";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  //shortcut useStatesnippet
-  // const [mapTask, setmapTask] = useState(tasks);
-
-  // useEffect(() => {
-  //     setmapTask(mapTask)
-  // }, [])
-
-  //shortcut clg
-  // console.log(mapTask)
-
-  // if (mapTask.length === 0) {
-  //   return <h1>there not tasks now</h1>;
-  // }
-
-  // function createTask(tasktitle, textDescription) {
-  //   // go ro send en props createTask
-
-  //   const newTask = {
-  //     title: tasktitle,
-  //     id: mapTask.length +1,
-  //     description: textDescription,
-  //   };
-
-  //   setmapTask([...mapTask, newTask]); // create new array tasks and add task, create new array no change the main
-  //   //update maptask
-
-  //   //other form is
-  //   // setmapTask([...mapTask, {
-  //   //   title: tasktitle,
-  //   //   id: mapTask.length,
-  //   //   description: "test create task"
-  //   // }])
-  // }
-
-  // function deleteTask(id){
-
-  //    setmapTask(mapTask.filter(task => task.id !== id))
-  //   // console.log(mapTask)
-  //   // console.log(id)
-
-  // }
-
-
+  const [count, setCount] = useState(0)
 
   return (
-    <div className="bg-zinc-900 h-screen">
-      <div className="container mx-auto p-4">
-          {/* <TaskForm createTask={createTask} /> */}
-          <TaskForm />
-          {/* <TaskList tasks={mapTask} deleteTask = {deleteTask} /> */}
-          <TaskList />
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-    </div>
-  );
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { AppContextProvider, AppContex } from './contex/AppContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppContextProvider>
-      <App />
-    </AppContextProvider>
+    <App />
   </React.StrictMode>,
 )
